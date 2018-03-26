d155a6b1db61da225e60fd8d5433fd6ca87372a1
kodama96 <g00338592@gmit.ie>
2018-03-26 18:23:43 +0100

    added specials and stacks (from Ian's video lecture)

--- shunt.go
+++ shunt.go
@@ -1,1 +1,33 @@
-  
+package main
+
+import (
+    "fmt"
+)
+
+func intopost(infix string) string {
+    specials := map[rune]int{'*': 10, '.': 9, '|': 8}
+
+    
+
+    pofix, s := []rune{}, []rune{} //s = stack
+    return string(pofix)
+}
+
+func main() {
+
+    // Answer: ab.c
+    fmt.Println("infix: ", "a.b.c*")
+    fmt.Println("postfix: ", intopost("a.b.c*"))
+
+    // Answer: abd|.*
+    fmt.Println("infix: ", "(a.(b|d))*")
+    fmt.Println("postfix: ", intopost("(a.(b|d))"))
+
+    // Answer: abd|.c*.
+    fmt.Println("infix ", "a.(b|d).c*")
+    fmt.Println("postfix ", intopost("a.(b|d).c*"))
+ 
+    //Answer: abb.+.c.
+    fmt.Println("infix ", "a.(b.b)+.c")
+    fmt.Println("postfix ", intopost("a.(b.b)+.c"))
+}
